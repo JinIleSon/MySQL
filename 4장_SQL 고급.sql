bf84d8276fb333b620add5c53041b8cc0c450540
thswlsdlf <sy7521012@naver.com>
2025-08-08 12:44:01 +0900

    modify 4장_SQL 고급

--- 4장_SQL 고급.sql
+++ 4장_SQL 고급.sql
@@ -86,15 +86,182 @@ INSERT INTO Sales (uid, year, month, sale) VALUES ('a104', 2020, 2,  84000);
 INSERT INTO Sales (uid, year, month, sale) VALUES ('a105', 2020, 2, 180000);
 INSERT INTO Sales (uid, year, month, sale) VALUES ('a108', 2020, 2,  76000);
 select * from sales;
+
 -- 실습 4-3
+select * from member where name='김유신';
+select * from member where pos='차장' and dep=101;
+select * from member where pos='차장' or dep=101;
+select * from member where name != '김춘추';
+select * from member where name <> '김춘추';
+select * from member where pos='사원' or pos='대리';
+select * from member where pos in ('사원', '대리');
+select * from member where dep in (101, 102, 103);
+select * from member where name like '%신';
+select * from member where name like '김%';
+select * from member where name like '김__';
+select * from member where name like '_성_';
+select * from member where name like '정_';
+select * from sales where sale > 50000;
+select * from sales where sale >= 50000 and sale < 100000 and month = 1;
+select * from sales where sale between 50000 and 100000;
+select * from sales where sale not between 50000 and 100000;
+select * from sales where year in (2020);
+select * from sales where month in (1, 2);
+
 -- 실습 4-4
+select * from sales order by sale;
+select * from sales order by sale asc;
+select * from sales order by sale desc;
+select * from member order by name;
+select * from member order by name desc;
+select * from member order by rdate asc;
+select * from sales where sale > 50000 order by sale desc;
+select * from sales where sale > 50000 order by year, month, sale desc;
+
 -- 실습 4-5
+select * from sales limit 3;
+select * from sales limit 0, 3;
+select * from sales limit 1, 2;
+select * from sales limit 5, 3;
+select * from sales order by sale desc limit 3, 5;
+select * from sales where sale < 50000 order by sale desc limit 3;
+select * from sales where sale > 50000 order by year desc, month, sale desc limit 5;
+
 -- 실습 4-6
+select sum(sale) as 합계 from sales;
+select avg(sale) as 평균 from sales;
+select max(sale) as 최대값 from sales;
+select min(sale) as 최소값 from sales;
+select ceiling(1.2);
+select ceiling(1.8);
+select floor(1.2);
+select floor(1.8);
+select round(1.2);
+select rand();
+select ceiling(rand() * 10);
+select count(sale) as 갯수 from sales;
+select count(*) as 갯수 from sales;
+
+select left('HelloWorld', 5);
+select right('HelloWorld', 5);
+select substring('HelloWorld', 6, 5);
+select concat('Hello', 'World');
+select concat(uid, name, hp) from member where uid='a108';
+
+select curdate();
+select curtime();
+select now();
+insert into member values ('a112', '유관순', '010-1234-1012', '대리', 107, now());
+
 -- 실습 4-7
+select sum(sale) as 총합 from sales where year=2018 and month=1;
+
 -- 실습 4-8
+select sum(sale) as 총합, avg(sale) as 평균 from sales where year=2019 and month=2 and sale >= 50000;
+
 -- 실습 4-9
+select min(sale) as 최저, max(sale) as 최고 from sales where year=2020;
+
 -- 실습 4-10
+select * from sales;
+
+select uid from sales group by uid;
+SELECT year FROM Sales GROUP BY year;
+SELECT  uid, year FROM Sales GROUP BY uid, year;
+select uid, count(*) as 건수 from sales group by uid;
+select uid, sum(sale) as 합계 from sales group by uid;
+select uid, avg(sale) as 평균 from sales group by uid;
+
+select uid, year, sum(sale) as 합계 from sales group by uid, year;
+select uid, year, sum(sale) as 합계 from sales group by uid, year order by year asc, 합계 desc;
+select uid, year, sum(sale) as 합계 from sales where sale >= 50000 group by uid, year order by 합계 desc;
+
 -- 실습 4-11
+select uid, sum(sale) as 합계 from sales group by uid having 합계 >= 200000;
+
+select uid, year, sum(sale) as 합계 from sales where sale >= 100000 group by uid, year having 합계 >= 200000 order by 합계 desc;
+
 -- 실습 4-12
+create table sales2 like sales;
+insert into sales2 select * from sales;
+select * from sales2;
+
+set SQL_SAFE_UPDATES = 0;
+update sales2 set year = year + 3;
+
+select * from sales union select * from sales2;
+select * from sales where sale >= 100000 union select * from sales2 where sale >= 100000;
+
+select uid, year, sale from sales union select uid, year, sale from sales2;
+
+select uid, year, sum(sale) as 합계 from sales
+group by uid, year
+union
+select uid, year, sum(sale) as 합계 from sales2
+group by uid, year
+order by year asc, 합계 desc;
+
 -- 실습 4-13
+select * from sales inner join member on sales.uid = member.uid;
+select * from member inner join department on member.dep = department.depno;
+
+select * from sales as a join member as b on a.uid = b.uid;
+select * from member as a join department as b on a.dep = b.depno;
+
+select * from sales as a, member as b where a.uid = b.uid;
+select * from member as a, department as b where a.dep = b.depno;
+
+select a.seq, a.uid, sale, name, pos from sales as a
+join member as b on a.uid = b.uid;
+
+select a.seq, a.uid, sale, name, pos from sales as a
+join member as b using(uid);
+
+select a.seq, a.uid, sale, name, pos from sales as a
+join member as b on a.uid = b.uid
+where sale >= 100000;
+
+SELECT a.seq, a.uid, b.name, b.pos, year, SUM(sale) AS 합계 FROM Sales AS a
+JOIN Member AS b ON a.uid = b.uid
+GROUP BY a.uid, a.year HAVING 합계 >= 100000
+ORDER BY a.year ASC, 합계 DESC;
+
+SELECT * FROM Sales AS a
+JOIN Member AS b ON a.uid = b.uid
+JOIN Department AS c ON b.dep = c.depNo;
+
+SELECT a.seq, a.uid, a.sale, b.name, b.pos, c.name FROM Sales AS a
+JOIN Member AS b ON a.uid = b.uid
+JOIN Department AS c ON b.dep = c.depNo;
+
+SELECT a.seq, a.uid, a.sale, b.name, b.pos, c.name FROM Sales AS a
+JOIN Member AS b ON a.uid = b.uid
+JOIN Department AS c ON b.dep = c.depNo
+WHERE sale > 100000
+ORDER BY sale DESC;
+
+-- 실습 4-14
+select * from sales as a left join member as b on a.uid = b.uid;
+select * from sales as a right join member as b on a.uid = b.uid;
+select a.seq, a.uid, sale, name, pos from sales as a left join member as b using(uid);
+
+select a.seq, a.uid, sale, name, pos from sales as a right join member as b using(uid);
+
+-- 실습 4-15
+select uid, m.name, pos, d.name
+from member as m join department as d
+on m.dep = d.depno;
+
+-- 실습 4-16
+select sum(sale) as "매출의 합" from sales as s
+join member as m
+on s.uid = m.uid where name = '김유신';
+
+-- 실습 4-17
+select m.name, d.name, pos, year, sum(sale) as "매출의 합" from sales s
+join member m on s.uid=m.uid 
+join department d on m.dep = d.depno
+where year='2019' and sale >= 50000 group by s.uid having sum(sale) >= 100000
+order by sum(sale) desc;
+
 
